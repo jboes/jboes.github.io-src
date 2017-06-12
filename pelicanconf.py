23e4ea6cf4be76f886792b6606c1f04d5d97ece2
Jacob Boes <jacobboes@gmail.com>
2017-06-11 20:51:56 -0700

    init

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,6 +7,22 @@ SITENAME = u"Boes' Blog"
 SITEURL = ''
 
 PATH = 'content'
+# static paths will be copied without parsing their contents
+STATIC_PATHS = [
+    'pdfs',
+    'images',]
+ARTICLE_PATHS = ['Blog']
+PAGE_PATHS = ['pages']
+ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{slug}.html'
+
+THEME = 'pelican-blueidea'
+THEME_STATIC_DIR = 'theme'
+SITESUBTITLE = 'Exploration through coding, catalysis, and education'
+GITHUB_URL = 'https://github.com/jboes'
+GOOGLE_ANALYTICS = 'UA-XXXX-YYYY'
+
+ORG_READER_EMACS_LOCATION = '/Applications/Emacs.app/Contents/MacOS/Emacs'
 
 TIMEZONE = 'US/Pacific'
 
@@ -20,16 +36,40 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('ORCID', 'https://orcid.org/0000-0002-7303-7782'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('GitHub', 'https://github.com/jboes'),
+          ('Twitter', 'https://twitter.com/jacob_boes'),
+          ('Linkedin', 'https://www.linkedin.com/in/jacobboes'),)
+
+PLUGIN_PATHS = ['/Users/jrboes/webpage/pelican-plugins']
+PLUGINS = ['org_reader']
 
 DEFAULT_PAGINATION = 10
+
+# Display pages list on the top menu
+DISPLAY_PAGES_ON_MENU = False
+
+# Display categories list on the top menu
+DISPLAY_CATEGORIES_ON_MENU = True
+
+# Display categories list as a submenu of the top menu
+DISPLAY_CATEGORIES_ON_SUBMENU = False
+
+# Display the category in the article's info
+DISPLAY_CATEGORIES_ON_POSTINFO = False
+
+# Display the author in the article's info
+DISPLAY_AUTHOR_ON_POSTINFO = False
+
+# Display the search form
+DISPLAY_SEARCH_FORM = False
+
+# Sort pages list by a given attribute
+# PAGES_SORT_ATTRIBUTE = Title
+
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
