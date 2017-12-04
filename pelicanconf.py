2350f5c7715b2e870de767d96cd13d217ce73c3a
Jacob Boes <jacobboes@gmail.com>
2017-12-03 19:31:03 -0800

    add scripts

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ SITEURL = ''
 
 PATH = 'content'
 # static paths will be copied without parsing their contents
-STATIC_PATHS = ['pdfs', 'images']
+STATIC_PATHS = ['pdfs', 'images', 'scripts']
 
 ARTICLE_PATHS = ['blog']
 PAGE_PATHS = ['pages']
